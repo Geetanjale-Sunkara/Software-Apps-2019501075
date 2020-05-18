de5d37c8ed3fcc194df9645cae445380f1e0a984
Geetanjale-Sunkara <geetanjale.sunkara@msitprogram.net>
2020-05-19 02:46:29 +0530

    app

--- todolist/todo.js
+++ todolist/todo.js
@@ -1,22 +1,23 @@
 function newTask() {
-    var li = document.createElement("li");
-    var inputValue = document.getElementById("myInput").value;
-    var t = document.createTextNode(inputValue);
-    li.appendChild(t);
-    if (inputValue === '') {
-        alert("You must write something!");
-    } else {
-        li.innerHTML += '<button onclick="doneTaskk(this)" >Task Done</button><button onclick="deleteTaskk(this)" >Delete Task</button>'
-        document.getElementById("mylist").appendChild(li);
-    }
-    document.getElementById("myInput").value = "";
+  var li = document.createElement("li");
+  var inputValue = document.getElementById("myInput").value;
+  var t = document.createTextNode(inputValue);
+  li.appendChild(t);
+  if (inputValue === "") {
+    alert("You must write something!");
+  } else {
+    li.innerHTML +=
+      '<button onclick="doneTaskk(this)" >Task Done</button><button onclick="deleteTaskk(this)" >Delete Task</button>';
+    document.getElementById("mylist").appendChild(li);
+  }
+  document.getElementById("myInput").value = "";
 }
 
 function deleteTaskk(ev) {
-    ev.parentNode.parentNode.removeChild(ev.parentNode);
+  ev.parentNode.parentNode.removeChild(ev.parentNode);
 }
 
-function doneTaskk(ev) {
-    ev.parentNode.style.textDecoration = "line-through";
-    ev.parentNode.removeChild(ev);
+function doneTaskk(eve) {
+  ev.parentNode.style.textDecoration = "line-through";
+  ev.parentNode.removeChild(eve);
 }
